3e6bbb4786027ae2a4be5f5d1816cf7c76ac28a8
tjordan4 <tysoncjordan@gmail.com>
2024-03-14 08:55:53 -0600

    added ice cream table in seed

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -1,85 +1,97 @@
-const db = require('./client');
-const { createUser } = require('./users');
+const db = require("./client");
+const { createUser } = require("./users");
 
 const users = [
   {
-    name: 'Emily Johnson',
-    email: 'emily@example.com',
-    password: 'securepass',
+    name: "Emily Johnson",
+    email: "emily@example.com",
+    password: "securepass",
   },
   {
-    name: 'Liu Wei',
-    email: 'liu@example.com',
-    password: 'strongpass',
+    name: "Liu Wei",
+    email: "liu@example.com",
+    password: "strongpass",
   },
   {
-    name: 'Isabella García',
-    email: 'bella@example.com',
-    password: 'pass1234',
+    name: "Isabella García",
+    email: "bella@example.com",
+    password: "pass1234",
   },
   {
-    name: 'Mohammed Ahmed',
-    email: 'mohammed@example.com',
-    password: 'mysecretpassword',
+    name: "Mohammed Ahmed",
+    email: "mohammed@example.com",
+    password: "mysecretpassword",
   },
   {
-    name: 'John Smith',
-    email: 'john@example.com',
-    password: 'password123',
+    name: "John Smith",
+    email: "john@example.com",
+    password: "password123",
   },
   // Add more user objects as needed
-];  
+];
 
 const dropTables = async () => {
-    try {
-        await db.query(`
+  try {
+    await db.query(`
         DROP TABLE IF EXISTS users;
-        `)
-    }
-    catch(err) {
-        throw err;
-    }
-}
+        `);
+  } catch (err) {
+    throw err;
+  }
+};
 
 const createTables = async () => {
-    try{
-        await db.query(`
+  try {
+    await db.query(`
         CREATE TABLE users(
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) DEFAULT 'name',
             email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL
-        )`)
-    }
-    catch(err) {
-        throw err;
-    }
-}
+        )`);
+
+    await db.query(`
+        CREATE TABLE iceCream(
+          id SERIAL PRIMARY KEY,
+          flavor TEXT,
+          brand TEXT,
+          price DECIMAL(2),
+          size TEXT,
+          imageUrl TEXT,
+          nutrition TEXT
+        )
+        `);
+  } catch (err) {
+    throw err;
+  }
+};
 
 const insertUsers = async () => {
   try {
     for (const user of users) {
-      await createUser({name: user.name, email: user.email, password: user.password});
+      await createUser({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      });
     }
-    console.log('Seed data inserted successfully.');
+    console.log("Seed data inserted successfully.");
   } catch (error) {
-    console.error('Error inserting seed data:', error);
+    console.error("Error inserting seed data:", error);
   }
 };
 
 const seedDatabse = async () => {
-    try {
-        db.connect();
-        await dropTables();
-        await createTables();
-        await insertUsers();
-    }
-    catch (err) {
-        throw err;
-    }
-    finally {
-        db.end()
-    }
-}
+  try {
+    db.connect();
+    await dropTables();
+    await createTables();
+    await insertUsers();
+  } catch (err) {
+    throw err;
+  } finally {
+    db.end();
+  }
+};
 
-seedDatabse()
+seedDatabse();
