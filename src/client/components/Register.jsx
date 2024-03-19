33b8be2ce28e2ce0651415e13ba35804a329f8cf
Jewwst <pegasus_hurcules@yahoo.com>
2024-03-19 10:00:14 -0500

    updated css for registration page, added effects for button hover

--- src/client/components/Register.jsx
+++ src/client/components/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./register.css";
 
 export default function AddUser({ setToken }) {
   const navigate = useNavigate();
