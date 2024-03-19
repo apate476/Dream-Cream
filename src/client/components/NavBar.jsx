266b9215705d2ff7f66250e21cb942051c784ac1
mustafa Jabr <jabrmustafa@gmail.com>
2024-03-19 14:49:04 -0400

    creat SearchBar

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -1,13 +1,9 @@
 import { Link } from "react-router-dom";
 import React, { useState } from 'react';
-import SearchBar from './SearchBar'; // Assuming SearchBar is in the same directory
+
 
 export default function NavBar() {
-  const handleSearch = (searchTerm) => {
-    // Implement your search logic here
-    // For example, redirect to a search results page or filter content on the current page
-    console.log(`Search Term: ${searchTerm}`);
-  };
+
 
   return (
     <nav>
@@ -16,6 +12,7 @@ export default function NavBar() {
       <Link to="api/users/register">Register</Link>
       <Link to="/about-us">About Us</Link>
       <Link to="">Cart</Link>
+      <Link to="/SearchBar">Search</Link>
     </nav>
   );
 }
