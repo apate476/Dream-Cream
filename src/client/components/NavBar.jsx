bc70c2081a96452562bfb836ac944a1c3eebbd52
Michael Denness <thisisturboproductions@gmail.com>
2024-03-19 14:39:47 -0400

    Fixed Routing for Checkout

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
       <Link to="/">Home</Link>
       <Link to="/api/users/login">Login</Link>
       <Link to="api/users/register">Register</Link>
-      <Link to="/api/orders_products">Checkout</Link>
+      <Link to="/api/orders/checkout">Checkout</Link>
       <Link to="/about-us">About Us</Link>
     </nav>
   );
